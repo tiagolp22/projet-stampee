f46f61519abd632818340d3e25bc7348e1fc946d
Clara Quintela <claraquintela@gmail.com>
2024-01-24 10:48:40 -0500

    continuation de la navigation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,7 @@
-console.log("test");
+const nav = document.querySelector('nav');
+
+nav.addEventListener("click", function(){
+    const isOpen = nav.dataset.opened === "true"
+    
+    nav.dataset.opened = !isOpen; 
+})
