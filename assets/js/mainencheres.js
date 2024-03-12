d7b08191403ac0b67a36b8fb3c030d5dddb5b617
René de Montigny <e2395944@cmaisonneuve.qc.ca>
2024-03-12 12:38:47 -0400

    Ajouter les fichiers JS et les questions UX

---
+++ assets/js/mainencheres.js
@@ -0,0 +1,6 @@
+import { Navigation } from "./navigation.js";
+
+
+// Pour lancer des comportements sur la navigation
+const nav = document.querySelector('[data-js-navigation]');
+new Navigation(nav);
